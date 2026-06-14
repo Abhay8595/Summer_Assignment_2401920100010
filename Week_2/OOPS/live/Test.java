965a80c2e8c8a2176f96694735677847b3223c16
Abhay8595 <abhaybhati374@gmail.com>
2026-06-14 23:38:11 +0530

    Add Test class to demonstrate Playable interface

--- Week_2/OOPS/live/Test.java
+++ Week_2/OOPS/live/Test.java
@@ -1,1 +1,21 @@
+package live;
 
+import music.Playable;
+import music.string.Veena;
+import music.wind.Saxophone;
+
+public class Test {
+    public static void main(String[] args) {
+        Veena veena = new Veena();
+        veena.play();
+
+        Saxophone saxophone = new Saxophone();
+        saxophone.play();
+
+        Playable playable1 = veena;
+        playable1.play();
+
+        Playable playable2 = saxophone;
+        playable2.play();
+    }
+}
