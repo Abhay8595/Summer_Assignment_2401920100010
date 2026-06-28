3ce0f01827b4e047834576c5b50348d4a55861b4
Abhay8595 <abhaybhati374@gmail.com>
2026-06-29 00:11:57 +0530

    Add outter class with inner class display methods

---
+++ Week_4/OOPS/outter.java
@@ -0,0 +1,11 @@
+public class outter{
+    public void display(){
+        System.out.println("display by outter");
+    }
+
+    class inner{
+        public void display(){
+            System.out.println("display by inner");
+        }
+    }
+}
