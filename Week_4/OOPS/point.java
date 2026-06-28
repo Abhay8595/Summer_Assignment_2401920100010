c49f47cf16ccfa95615db28e39afa2d5bc32ea0b
Abhay8595 <abhaybhati374@gmail.com>
2026-06-29 00:11:25 +0530

    Add point class with x and y coordinates

---
+++ Week_4/OOPS/point.java
@@ -0,0 +1,26 @@
+public class point
+{
+    private int x;
+    private int y;
+    public point(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public point(){
+
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    public void setXY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
